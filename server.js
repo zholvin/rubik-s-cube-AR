be6638c59882846c233c70814df6036273e8313e
Junjie <122109215@umail.ucc.ie>
2025-02-23 01:29:56 +0000

    Refactor(file-organization): Reorganize and clean up files for better readability
    - Move HTML and static resources from `cube-controller` folder to `public` folder for better organization
    - Rename and rearrange cube controller HTML files to improve readability
    - Delete unused and broken HTML files, remove unnecessary model files
    - Add three buttons in the `homepage` HTML for debugging, allowing quick access to AR and VR test links
    
    Impact:
    - Improves file structure and usability for debugging purposes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/templates/junjiehome.html'));
+    res.sendFile(path.join(__dirname, 'public/pages/01-homepage/junjiehome.html'));
 });
 app.listen(8080, () => {
     console.log("Listening on http://localhost:8080");
